04d5421b60ac9453fcd004799f426c5bc9968d34
EC2 Default User <ec2-user@ip-172-31-25-202.us-east-2.compute.internal>
2018-09-04 02:32:35 +0000

    have

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -14,6 +14,12 @@ class OwnershipsController < ApplicationController
     if params[:type] == 'Want'
       current_user.want(@item)
       flash[:success] = '商品を Want しました。'
+      
+    else
+      params[:type] == 'Have'
+      current_user.have(@item)
+      flash[:success] = '商品を Have しました。'
+      
     end
 
     redirect_back(fallback_location: root_path)
@@ -25,6 +31,12 @@ class OwnershipsController < ApplicationController
     if params[:type] == 'Want'
       current_user.unwant(@item) 
       flash[:success] = '商品の Want を解除しました。'
+      
+    else
+      params[:type] == 'Have'
+      current_user.nothave(@item) 
+      flash[:success] = '商品の Have を解除しました。'
+      
     end
 
     redirect_back(fallback_location: root_path)
