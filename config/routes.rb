9c9d8a730809ca652f396260c275db1164444c81
EC2 Default User <ec2-user@ip-172-31-25-202.us-east-2.compute.internal>
2018-09-04 00:17:31 +0000

    want

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root to: 'toppages#index'
-  
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  
+
   get 'signup', to: 'users#new'
   resources :users, only: [:show, :new, :create]
-  
-  resources :items, only: [:new]
+
+  resources :items, only: [:show, :new]
+  resources :ownerships, only: [:create, :destroy]
   
 end
